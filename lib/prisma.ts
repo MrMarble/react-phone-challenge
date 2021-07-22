093d39a79ddb27b3613c20160d190dec9d5cb822
Alvaro Tinoco <alv_tinoco_97@hotmail.com>
2021-07-22 17:27:29 +0200

    fix(types): globalThis

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,14 +1,18 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
 let prisma: PrismaClient;
 
-if (process.env.NODE_ENV === 'production') {
+declare global {
+  var prismaClient: PrismaClient;
+}
+
+if (process.env.NODE_ENV === "production") {
   prisma = new PrismaClient();
 } else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
+  if (!global.prismaClient) {
+    global.prismaClient = new PrismaClient();
   }
-  prisma = global.prisma;
+  prisma = global.prismaClient;
 }
 
 export default prisma;
