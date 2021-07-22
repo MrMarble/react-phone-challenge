1d9f2d576e05af66a0c04000470053bf5f8fd80a
Alvaro Tinoco <alv_tinoco_97@hotmail.com>
2021-07-22 15:57:51 +0200

    feat(page): basic home

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,5 @@
-import '../styles/globals.css'
-
 import { ChakraProvider } from "@chakra-ui/react";
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
