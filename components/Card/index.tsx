38b5c813172372eb3facb3f30acc5879bdae3087
Alvaro Tinoco <alv_tinoco_97@hotmail.com>
2021-07-22 15:57:24 +0200

    feat(component): card component

---
+++ components/Card/index.tsx
@@ -0,0 +1,39 @@
+import { Badge, Box } from "@chakra-ui/react";
+import { Phone } from "@prisma/client";
+import Image from "next/image";
+
+export const Card = ({ phone }: { phone: Phone }) => (
+  <Box maxW="sm" borderWidth="1px" borderRadius="lg" overflow="hidden">
+    <Box minH="200" position="relative">
+      <Image
+        layout="fill"
+        src={phone.imageFileName || "/no_image.svg"}
+        alt={`${phone.manufacturer} ${phone.name}`}
+      />
+    </Box>
+
+    <Box p="6">
+      <Box d="flex" alignItems="baseline">
+        <Badge borderRadius="full" px="2" colorScheme="teal">
+          {phone.manufacturer}
+        </Badge>
+        <Box
+          color="gray.500"
+          fontWeight="semibold"
+          letterSpacing="wide"
+          fontSize="xs"
+          textTransform="uppercase"
+          ml="2"
+        >
+          {phone.screenType} &bull; {phone.ram}
+        </Box>
+      </Box>
+
+      <Box mt="1" fontWeight="semibold" as="h4" lineHeight="tight" isTruncated>
+        {phone.name}
+      </Box>
+
+      <Box>{phone.price}</Box>
+    </Box>
+  </Box>
+);
