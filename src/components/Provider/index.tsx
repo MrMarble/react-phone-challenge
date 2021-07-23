64b3abc4af111e3a62aac55d0bd6be9eb3c1d34d
Alvaro Tinoco <alv_tinoco_97@hotmail.com>
2021-07-23 12:35:57 +0200

    refactor(provider): remove unused prop

--- src/components/Provider/index.tsx
+++ src/components/Provider/index.tsx
@@ -10,19 +10,15 @@ import {
 type Context = {
   phoneCount: number;
   totalPhones: number;
-  filter: string;
   setPhoneCount: Dispatch<SetStateAction<number>>;
   setTotalPhones: Dispatch<SetStateAction<number>>;
-  setFilter: Dispatch<SetStateAction<string>>;
 };
 
 const PhoneContext = createContext<Context>({
-  filter: "",
   phoneCount: 0,
   totalPhones: 0,
   setPhoneCount: () => {},
   setTotalPhones: () => {},
-  setFilter: () => {},
 });
 
 export const usePhoneContext = () => useContext(PhoneContext);
@@ -30,14 +26,11 @@ export const usePhoneContext = () => useContext(PhoneContext);
 export default function PhoneProvider({ children }: { children: ReactChild }) {
   const [phoneCount, setPhoneCount] = useState(0);
   const [totalPhones, setTotalPhones] = useState(0);
-  const [filter, setFilter] = useState("");
 
   const value = {
     phoneCount,
     totalPhones,
-    filter,
     setPhoneCount,
-    setFilter,
     setTotalPhones,
   };
   return (
